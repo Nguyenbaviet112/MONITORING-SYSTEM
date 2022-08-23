d7adfda3d08db0af14eb9107b194c9ef4960161d
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-08-23 21:19:43 +0700

    First commit

---
+++ Server/src/controller/ServerListener.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.ServerView;
+
+public class ServerListener implements ActionListener {
+
+	private ServerView sv;
+	
+	public ServerListener(ServerView sv)
+	{
+		this.sv = sv;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String button = e.getActionCommand();
+		
+		if (button.equals("Start"))
+		{
+			this.sv.show_IP_Port();
+		}
+		
+	}
+
+}
